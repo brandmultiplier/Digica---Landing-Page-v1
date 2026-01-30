a1a3df774adba088ae919919dc49c6afe0a5c6ec
Sivasish Praharaj <sivasish48@gmail.com>
2026-01-30 22:00:13 +0530

    Change section name from 'Who is Digica' to 'Who are Digica'

--- components/Header.tsx
+++ components/Header.tsx
@@ -74,7 +74,7 @@ export const Header: React.FC = () => {
             onClick={(e) => handleScrollTo(e, 'about')}
             className="text-sm font-medium text-gray-600 hover:text-digica-red transition-colors"
           >
-            Who is Digica
+            Who are Digica
           </a>
           <a 
             href="#problem" 
@@ -120,7 +120,7 @@ export const Header: React.FC = () => {
             className="text-sm font-medium text-gray-600" 
             onClick={(e) => handleScrollTo(e, 'about')}
           >
-            Who is Digica
+            Who are Digica
           </a>
           <a 
             href="#problem" 
